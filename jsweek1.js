9af46ff0714ef3ba0146513eea9983380be3ebfa
priyamtalukdar <95258641+priyamtalukdar@users.noreply.github.com>
2022-08-24 09:01:20 +0530

    Add files via upload

---
+++ jsweek1.js
@@ -0,0 +1,38 @@
+let x= 10;
+console.log(x);
+let y=20;
+let z="hello";
+let a=true;
+let c=null;
+let b;
+const PI=3.14;
+console.log(x+y);
+console.log(x+z);
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log(x===y);
+console.log(x>=y);
+let myVar="Teaching int219"
+console.log(myVar.charAt(0));
+console.log(myVar.indexOf("int"));
+console.log(myVar.slice(1,9));
+console.log(myVar.toUpperCase());
+console.log(myVar.toLowerCase());
+console.log(myVar.includes("219"));
+console.log(myVar.includes("of"));
+
+console.log(Number.isInteger(myVar));
+console.log(Number.isInteger(y));
+console.log(Math.random());
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(Math.PI));
+console.log(Math.ceil(Math.PI));
+console.log(Math.floor(Math.PI));
+console.log(Math.random()*1000+1);
+console.log(myVar.charAt(Math.floor(Math.random()*String.length+1)));
